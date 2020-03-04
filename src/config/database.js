5e0eb35196df50663d01ec32b7e605708d42d421
acaciomartins <acacio.am@gmail.com>
2020-03-03 21:10:44 -0300

    Ajustes acesso database Heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
-    username: 'root',
-    password: 'root',
-    database: 'sis_cad',
-    host: 'localhost',
+    username: 'tyu32nhn3gw9bsg7',
+    password: 't4z1ywku9g4n8qum',
+    database: 'p43fpm87fpa4zelu',
+    host: 'ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     port: '3306',
     dialect: 'mysql',
 
