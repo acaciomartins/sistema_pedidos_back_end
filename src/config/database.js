d4b8b17bc81d184964cb6bd123e040a232e07bc3
root <acacio.am@gmail.com>
2020-04-16 08:32:49 -0300

    Ajustes acesso database

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,21 @@
+// module.exports = {
+//     username: 'tyu32nhn3gw9bsg7',
+//     password: 't4z1ywku9g4n8qum',
+//     database: 'p43fpm87fpa4zelu',
+//     host: 'ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//     port: '3306',
+//     dialect: 'mysql',
+
+//     define: {
+//         timestamps: true,
+//         underscored: true
+//     },
+// };
 module.exports = {
-    username: 'tyu32nhn3gw9bsg7',
-    password: 't4z1ywku9g4n8qum',
-    database: 'p43fpm87fpa4zelu',
-    host: 'ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    username: 'root',
+    password: 'root',
+    database: 'cadastro_pedidos',
+    host: 'localhost',
     port: '3306',
     dialect: 'mysql',
 
