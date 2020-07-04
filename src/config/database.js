3bf1525de1763019482562fcd7ee8d8c58aba069
Acácio Amorelli Martins <acacio.am@gmail.com>
2020-07-04 08:34:09 -0300

    Conexao heroky

--- src/config/database.js
+++ src/config/database.js
@@ -1,26 +1,26 @@
-// module.exports = {
-//     username: 'tyu32nhn3gw9bsg7',
-//     password: 't4z1ywku9g4n8qum',
-//     database: 'p43fpm87fpa4zelu',
-//     host: 'ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//     port: '3306',
-//     dialect: 'mysql',
+ module.exports = {
+     username: 'tyu32nhn3gw9bsg7',
+     password: 't4z1ywku9g4n8qum',
+    database: 'p43fpm87fpa4zelu',
+     host: 'ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+     port: '3306',
+     dialect: 'mysql',
 
-//     define: {
-//         timestamps: true,
-//         underscored: true
-//     },
+     define: {
+         timestamps: true,
+         underscored: true
+     },
 // };
-module.exports = {
-    username: 'root',
-    password: 'root',
-    database: 'cadastro_pedidos',
-    host: 'localhost',
-    port: '3306',
-    dialect: 'mysql',
+//module.exports = {
+ //   username: 'root',
+  //  password: 'root',
+   // database: 'cadastro_pedidos',
+    //host: 'localhost',
+    //port: '3306',
+//    dialect: 'mysql',
 
-    define: {
-        timestamps: true,
-        underscored: true
-    },
+ //   define: {
+ //       timestamps: true,
+  //      underscored: true
+ //   },
 };
